d9e8461c7873e378bd77c55660da48dee06aa111
SalawuJamiuOlatunji <salawujamiuolatunji60@gmail.com>
2026-01-13 09:59:10 +0100

    Initial Bloombreed College website

---
+++ src/pages/Academics.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function Academics() {
+  return (
+    <div>
+      <Navbar />
+      <section></section>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="py-16 px-8 max-w-5xl mx-auto text-center">
+        <h2 className="text-3xl font-bold text-green-700 mb-4">
+          Welcome to Bloombreed College
+        </h2>
+        <p className="text-gray-600">
+          Bloombreed College is committed to raising disciplined, intelligent
+          and confident students who are prepared for a better future. We
+          combine academic excellence with moral values to produce tomorrow’s
+          leaders.
+        </p>
+      </section>
+      <Footer />
+    </div>
+  );
+}
