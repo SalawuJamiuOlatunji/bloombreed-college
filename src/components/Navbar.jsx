3a5b61d88d27b9e4b7b7fc891984a92b25a2cd98
SalawuJamiuOlatunji <salawujamiuolatunji60@gmail.com>
2026-01-13 18:45:16 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,13 @@ import locked from "../assets/locked.png";
 export default function Navbar() {
   return (
     <nav className="bg-blue-500 shadow-md px-8 py-4 flex items-center justify-between">
+      {/* left side of navbar */}
       <div className="flex items-center gap-3">
-        <img src={locked} alt="Bloombreed Logo" className="h-10" />
+        <img
+          src={locked}
+          alt="Bloombreed Logo"
+          className="h-[100px] w-[100px] rounded-[10px]"
+        />
         <div>
           <h1 className="text-orange-400 font-bold text-xl">
             Bloombreed College
@@ -14,7 +19,29 @@ export default function Navbar() {
           </p>
         </div>
       </div>
-      <div></div>
+      {/* right side of navbar */}
+      <div className="border flex flex-col items-end border-lime-300 h-full w-[50%]">
+        <h1 className="text-white mb-2 font-bold text-xl text-right">
+          Phone: +2348023221910
+        </h1>
+        <div className="hidden mt-2 md:flex gap-8 text-gray-700 font-medium">
+          <a href="#" className="text-white hover:text-green-700">
+            Home
+          </a>
+          <a href="#" className="text-white hover:text-green-700">
+            About
+          </a>
+          <a href="#" className="text-white hover:text-green-700">
+            Admissions
+          </a>
+          <a href="#" className="text-white hover:text-green-700">
+            Gallery
+          </a>
+          <a href="#" className="text-white hover:text-green-700">
+            Contact
+          </a>
+        </div>
+      </div>
     </nav>
   );
 }
