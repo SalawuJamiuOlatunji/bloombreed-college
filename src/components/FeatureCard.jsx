420a3888b11563e0a19f99fe22a12c23a00872bb
SalawuJamiuOlatunji <salawujamiuolatunji60@gmail.com>
2026-01-15 20:09:52 +0100

    homepage

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,8 +1,8 @@
-export default function FeatureCard({title,description}){
-    return(
-        <div className="bg-white rounded-lg border  shadow-md py-20 px-6 text-center">
-            <h1 className="text-xl font-semibold mb-2">{title}</h1>
-            <p className="text-gray-500">{description}</p>
-        </div>
-    )
+export default function FeatureCard({ title, description }) {
+  return (
+    <div className="bg-white rounded-lg shadow-md py-15 px-6 text-center">
+      <h1 className="text-xl font-semibold mb-2">{title}</h1>
+      <p className="text-gray-500">{description}</p>
+    </div>
+  );
 }
