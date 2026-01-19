3000b093e05aa3382b6ea853d1f7fe5e947e532f
SalawuJamiuOlatunji <salawujamiuolatunji60@gmail.com>
2026-01-19 12:25:50 +0100

    bill

--- src/pages/Bill.jsx
+++ src/pages/Bill.jsx
@@ -1,7 +1,7 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
-export default function Careers() {
+export default function Bill() {
   return (
     <div>
       <Navbar />
