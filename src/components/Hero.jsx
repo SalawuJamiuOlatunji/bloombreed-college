40de4fa43e2118ff20f6b56f3f4f77b694a0a341
SalawuJamiuOlatunji <salawujamiuolatunji60@gmail.com>
2026-02-05 16:07:56 +0100

    mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section className="bg-gray-100 py-20 text-center">
-      <h1 className="text-5xl font-bold text-blue-700 mb-4">
+      <h1 className=" text-3xl md:text-5xl font-bold text-blue-700 mb-4">
         Raising Giants for a Better Future
       </h1>
       <p className="text-gray-600 max-w-xl mx-auto">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,45 +3,73 @@ import locked from "../assets/locked.png";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-500 shadow-md px-8 py-6 flex items-center justify-between">
-      {/* left side of navbar */}
-      <div className="flex items-center gap-3">
-        <img
-          src={locked}
-          alt="Bloombreed Logo"
-          className="size-20 rounded-[10px]"
-        />
+    <nav className="sm:justify-center sm:p-0   md:bg-blue-500 shadow-md md:px-8 md:py-6 ">
+      {/* Mobile below md */}
+      <div className="md:hidden border my-5 border-red-500 w-full h-auto flex flex-col justify-center items-center gap-3">
         <div>
-          <h1 className="text-orange-400 font-bold text-xl">
-            Bloombreed College
-          </h1>
-          <p className="text-orange-300 text-sm">
-            Raising giants for a better future
-          </p>
+          <span className="bg-orange-500 p-3 my-2 rounded text-white">
+            +2348023221910
+          </span>
+        </div>
+        <div className="flex items-center gap-3">
+          <img
+            src={locked}
+            alt="Bloombreed Logo"
+            className="size-15 rounded-[10px]"
+          />
+          <div>
+            <h1 className="text-orange-400 font-bold text-xl">
+              Bloombreed College
+            </h1>
+            <p className="text-orange-300 text-sm">
+              Raising giants for a better future
+            </p>
+          </div>
         </div>
       </div>
 
-      {/* right side of navbar */}
-      <div className="flex flex-col items-end w-[50%]">
-        <h1 className="text-white mb-2 font-bold text-xl text-right">
-          Phone: +2348023221910
-        </h1>
-        <div className="hidden mt-2 md:flex gap-8 font-medium">
-          <Link to="/" className="text-white hover:text-green-700">
-            Home
-          </Link>
-          <Link to="/about" className="text-white hover:text-green-700">
-            About
-          </Link>
-          <Link to="/admission" className="text-white hover:text-green-700">
-            Admission
-          </Link>
-          <Link to="/bills" className="text-white hover:text-green-700">
-            Bills
-          </Link>
-          <Link to="/information" className="text-white hover:text-green-700">
-            Information
-          </Link>
+      {/* md and above */}
+
+      <div className="hidden md:flex md:flex md:items-center md:justify-between">
+        {/* left side of navbar */}
+        <div className="flex items-center gap-3">
+          <img
+            src={locked}
+            alt="Bloombreed Logo"
+            className="size-20 rounded-[10px]"
+          />
+          <div>
+            <h1 className="text-orange-400 font-bold text-xl">
+              Bloombreed College
+            </h1>
+            <p className="text-orange-300 text-sm">
+              Raising giants for a better future
+            </p>
+          </div>
+        </div>
+
+        {/* right side of navbar */}
+        <div className="flex flex-col items-end w-[50%]">
+          <h1 className="text-white mb-2 font-bold text-xl text-right">
+            Phone: +2348023221910
+          </h1>
+          <div className="hidden mt-2 md:flex gap-8 font-medium">
+            <Link to="/" className="text-white hover:text-green-700">
+              Home
+            </Link>
+            <Link to="/about" className="text-white hover:text-green-700">
+              About
+            </Link>
+            <Link to="/admission" className="text-white hover:text-green-700">
+              Admission
+            </Link>
+            <Link to="/bills" className="text-white hover:text-green-700">
+              Bills
+            </Link>
+            <Link to="/information" className="text-white hover:text-green-700">
+              Information
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
